fbab91efaae6c5879f8600fc2a57968c11494665
UmayAyman <aimanumme827@gmail.com>
2025-02-18 15:22:31 +0500

    Add files via upload

---
+++ data/blogs.js
@@ -0,0 +1,27 @@
+blogs = [
+    {
+        title: "how to get started with Python",
+        content: "this is Python content",
+        slug: "Python-learn"
+    },
+
+    {
+        title: "how to get started with JavaScript",
+        content: "this is JavaScript content",
+        slug: "JavaScript-learn"
+    },
+
+    {
+        title: "how to get started with Django",
+        content: "this is Django content",
+        slug: "Django-learn"
+    },
+
+    {
+        title: "how to get started with CSS",
+        content: "this is Css content",
+        slug: "Css-learn"
+    }
+]
+
+module.exports = blogs;
